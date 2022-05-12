92aebdd408c5494259420517f732b1b1fd0815d7
mikh-maksi <newlevelcenter@gmail.com>
2022-05-12 18:10:49 +0300

    geometry02

---
+++ math2IT/div_sign/python/div_sign.py
@@ -0,0 +1,9 @@
+str01 = "123hjkhsdf kjhdfk j lkjshdkjfh lkjlkjd sdlfkj l ldskjflkj l;kjsdlkfj l;klkjsdf lkj lkjsdf lkj  lsdkjfl kjlksjdf lkj l"
+str02 = ''
+n=1
+for el in str01:
+    str02+=el
+    if (n%10==0):
+        str02+='\r\n'
+    n+=1
+print(str02)
